de2b1fb41a7cca7e938d0b178d8fc632bd02579a
ayushmetaverse <ayushmt3@gmail.com>
2024-05-04 17:23:38 +0530

    First Test  Commite
    This is the first Commit thats Setup the project and Get things get stareted

---
+++ NearByMe/NearByMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearByMe
+//
+//  Created by apple on 04/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
